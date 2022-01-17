617c092bd99f9143ee5aeb3f8267acced7feb827
mdeias <mdeias@icloud.com>
2022-01-17 16:11:46 +0100

    first commit

---
+++ Movie.php
@@ -0,0 +1,12 @@
+<?php
+class Movie {
+    public $title;
+    public $date;
+    public $athor;
+
+
+    function __construct($_title) {
+        $this->title = $_title;
+    }
+
+}
---
+++ database.php
@@ -0,0 +1,39 @@
+<?php
+
+$movies = [
+
+    [
+        'title' => 'Avengers: Endgame',
+        'date' => 2019,
+        'author' => 'Antony Russo, Joe Russo',
+        'description' => 'Alla deriva nello spazio senza cibo o acqua, Tony Stark vede la propria scorta di ossigeno diminuire di minuto in minuto. Nel frattempo, i restanti Vendicatori affrontano un epico scontro con Thanos.',
+        'poster'=> 'https://pad.mymovies.it/filmclub/2018/12/029/locandinapg2.jpg'
+    ],
+
+
+    [
+        'title' => 'Eternals',
+        'date' => 2021,
+        'author' => 'Chloé Zhao',
+        'description' => 'Dopo gli eventi accaduti in Avengers: Endgame, una tragedia inaspettata obbliga gli Eterni a uscire dall ombra e riunire le forze contro il più antico dei nemici dell umanità, i Devianti.',
+        'poster' => 'https://pad.mymovies.it/filmclub/2019/05/044/locandinapg1.jpg'
+    ],
+
+    [
+        'title' => 'Doctor Strange',
+        'date' => 2016,
+        'author' => 'Scott Derrickson',
+        'description' => 'Un medico resta vittima di un tragico incidente, ma viene aiutato da uno stregone che lo inizia alle arti magiche, grazie alle quali egli riesce a redimersi e a combattere le forze oscure che minacciano l umanità.',
+        'poster' => 'https://pad.mymovies.it/filmclub/2010/06/111/locandina.jpg'
+    ],
+
+    [
+        'title' => 'Iron Man 3',
+        'date' => 2013,
+        'author' => 'Shane Black',
+        'description' => 'Dopo aver salvato New York dalla distruzione ed essere rimasto da allora insonne e preoccupato, Tony Stark è rimasto senza armatura e deve lottare contro le sue paure interiori per sconfiggere il suo nuovo nemico, il Mandarino.',
+        'poster' => 'https://pad.mymovies.it/filmclub/2012/06/004/locandinapg1.jpg'
+    ],
+
+
+];
