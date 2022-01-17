fadbff83c05fd1fe652d5997520e72d06d25ebc4
mdeias <mdeias@icloud.com>
2022-01-17 16:36:52 +0100

    stampa con foreach

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<?php
 
+   require_once __DIR__ . "/database.php";
+   require_once __DIR__ . "/Movie.php";
 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +14,24 @@
     <title>Movie</title>
 </head>
 <body>
-    
+
+    <?php foreach ($movies as $movie):
+    $new_movie = new Movie($movie['title']);
+    $new_movie->date = $movie['date'];
+    $new_movie->author = $movie['author'];
+    $new_movie->description = $movie['description'];
+    $new_movie->poster = $movie['poster'];
+    ?>
+
+    <div class="card">
+        <img src="<?php echo $new_movie->poster ?>" alt="">
+        <h2> <?php echo $new_movie->title; ?> </h2>
+        <h4> <?php echo $new_movie->author; ?> </h4>
+        <h5> <?php echo $new_movie->date; ?> </h5>
+        <p> <?php echo $new_movie->description; ?> </p>
+    </div>
+
+    <?php endforeach; ?>
+
 </body>
 </html>
